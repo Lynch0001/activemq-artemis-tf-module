5d23a0fd44c58c3b2e40d428db40ea9fbe4ecc4e
Tim Lynch <lynch0001@gmail.com>
2023-10-15 05:56:15 -0700

    reset deployment-base

--- charts/templates/delete_all_broker.sh
+++ charts/templates/delete_all_broker.sh
@@ -2,7 +2,6 @@
 
 export PATH=~/.minikube/cache/linux/amd64/v1.27.3:$PATH
 
-kubectl delete -f namespace.yaml
 kubectl delete -f secret-certs.yaml
 kubectl delete -f configmap.yaml
 kubectl delete -f configmap-bootstrap.yaml
@@ -17,3 +16,4 @@ kubectl delete -f configmap-users.yaml
 kubectl delete -f deployment-base.yaml
 kubectl delete -f service-internal.yaml
 kubectl delete -f service-external.yaml
+kubectl delete -f namespace.yaml
